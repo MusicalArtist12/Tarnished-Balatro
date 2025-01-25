e5d883c34fbd757a77c7fe7650ab29f9d5f153d4
MusicalArtist12 <>
2025-01-24 23:59:43 -0800

    work in progress, rest of suits

--- Tarnished.lua
+++ Tarnished.lua
@@ -20,6 +20,21 @@ SMODS.Atlas {
     py = 95
 }
   
+SMODS.Atlas {
+    key = "LC_ui",
+    path = "lc_ui.png",
+    px = 18,
+    py = 18
+}
+  
+
+SMODS.Atlas {
+    key = "HC_ui",
+    path = "hc_ui.png",
+    px = 18,
+    py = 18
+}
+  
 
 
 local function has_any_enhancement(card)
@@ -38,7 +53,7 @@ SMODS.Joker {
     loc_txt = {
         name = "Fragile Heart",
         text = {
-            "Enhances played {C:red}Hearts{}", -- [[todo]] use localized value
+            "Enhances played {C:red}Hearts{}", -- [[todo]] use localized value and color
             "{C:green}#1# in #2#{} chance played {C:red}heart{}",
             "is destroyed"
         }
@@ -53,6 +68,8 @@ SMODS.Joker {
         return { vars = { (G.GAME.probabilities.normal or 1), card.ability.extra.odds } }
     end,
     calculate = function(self, card, context)
+        
+        -- check for null?
         -- score, then add enhancement
         if context.individual and context.cardarea == G.play then
             if context.other_card:is_suit('Hearts') and not has_any_enhancement(context.other_card) then
@@ -76,15 +93,67 @@ SMODS.Joker {
 SMODS.Suit {
     key = "broken-heart",
     card_key = "BH",
-    pos = { y = 0 },
-    ui_pos = 5,
+    pos = { x = 0, y = 0 },
+    ui_pos = { x = 0, y = 1 },
     loc_txt = {
         singular = "Broken Heart",
         plural = "Broken Hearts"
     },
     lc_atlas = 'LC_deck',
-    hc_atlas = 'HC_deck'
+    hc_atlas = 'HC_deck',
+    lc_ui_atlas = 'LC_ui',
+    hc_ui_atlas = 'HC_ui'
+    -- [[todo]]: give this a color
 }
+
+SMODS.Suit {
+    key = "overgrown-club",
+    card_key = "OC",
+    pos = { x = 0, y = 1 },
+    ui_pos = { x = 1, y = 1 },
+    loc_txt = {
+        singular = "Overgrown Club",
+        plural = "Overgrown Clubs"
+    },
+    lc_atlas = 'LC_deck',
+    hc_atlas = 'HC_deck',
+    lc_ui_atlas = 'LC_ui',
+    hc_ui_atlas = 'HC_ui'
+    -- [[todo]]: give this a color
+}
+
+SMODS.Suit {
+    key = "blood-diamond",
+    card_key = "BD",
+    pos = { x = 0, y = 2 },
+    ui_pos = { x = 2, y = 1 },
+    loc_txt = {
+        singular = "Blood Diamond",
+        plural = "Blood Diamonds"
+    },
+    lc_atlas = 'LC_deck',
+    hc_atlas = 'HC_deck',
+    lc_ui_atlas = 'LC_ui',
+    hc_ui_atlas = 'HC_ui'
+    -- [[todo]]: give this a color
+}
+
+SMODS.Suit {
+    key = "rusty-spade",
+    card_key = "RS",
+    pos = { x = 0, y = 3 },
+    ui_pos = { x = 3, y = 1 },
+    loc_txt = {
+        singular = "Rusty Spade",
+        plural = "Rusty Spades"
+    },
+    lc_atlas = 'LC_deck',
+    hc_atlas = 'HC_deck',
+    lc_ui_atlas = 'LC_ui',
+    hc_ui_atlas = 'HC_ui'
+    -- [[todo]]: give this a color
+}
+
 
 -- serves to test features, comment out before creating a release
 SMODS.Challenge {
@@ -121,8 +190,48 @@ SMODS.Challenge {
             { s = 'tarnished_BH', r = "4" }, 
             { s = 'tarnished_BH', r = "3" }, 
             { s = 'tarnished_BH', r = "2" }, 
-            { s = 'tarnished_BH', r = "A" }, 
-    
+
+            { s = 'tarnished_OC', r = "A" }, 
+            { s = 'tarnished_OC', r = "K" }, 
+            { s = 'tarnished_OC', r = "Q" }, 
+            { s = 'tarnished_OC', r = "J" }, 
+            { s = 'tarnished_OC', r = "T" }, 
+            { s = 'tarnished_OC', r = "9" }, 
+            { s = 'tarnished_OC', r = "8" }, 
+            { s = 'tarnished_OC', r = "7" }, 
+            { s = 'tarnished_OC', r = "6" }, 
+            { s = 'tarnished_OC', r = "5" }, 
+            { s = 'tarnished_OC', r = "4" }, 
+            { s = 'tarnished_OC', r = "3" }, 
+            { s = 'tarnished_OC', r = "2" }, 
+
+             { s = 'tarnished_BD', r = "A" }, 
+             { s = 'tarnished_BD', r = "K" }, 
+             { s = 'tarnished_BD', r = "Q" }, 
+             { s = 'tarnished_BD', r = "J" }, 
+             { s = 'tarnished_BD', r = "T" }, 
+             { s = 'tarnished_BD', r = "9" }, 
+             { s = 'tarnished_BD', r = "8" }, 
+             { s = 'tarnished_BD', r = "7" }, 
+             { s = 'tarnished_BD', r = "6" }, 
+             { s = 'tarnished_BD', r = "5" }, 
+             { s = 'tarnished_BD', r = "4" }, 
+             { s = 'tarnished_BD', r = "3" }, 
+             { s = 'tarnished_BD', r = "2" }, 
+        
+             { s = 'tarnished_RS', r = "A" }, 
+             { s = 'tarnished_RS', r = "K" }, 
+             { s = 'tarnished_RS', r = "Q" }, 
+             { s = 'tarnished_RS', r = "J" }, 
+             { s = 'tarnished_RS', r = "T" }, 
+             { s = 'tarnished_RS', r = "9" }, 
+             { s = 'tarnished_RS', r = "8" }, 
+             { s = 'tarnished_RS', r = "7" }, 
+             { s = 'tarnished_RS', r = "6" }, 
+             { s = 'tarnished_RS', r = "5" }, 
+             { s = 'tarnished_RS', r = "4" }, 
+             { s = 'tarnished_RS', r = "3" }, 
+             { s = 'tarnished_RS', r = "2" }, 
         }
     }
 }
